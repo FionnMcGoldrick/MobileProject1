d9af01b1541e27e0a1b0ce4a16ded5367305de24
Fionn McGoldrick <G00422349@atu.com>
2024-05-10 02:46:29 +0000

    Added components correctly

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,19 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { GamingService } from '../services/gaming.service';
+import { Component } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonLabel } from '@ionic/angular/standalone';
+import { IonItem } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.page.html',
-  styleUrls: ['./home.page.scss'],
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: true,
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel],
 })
-export class HomePage implements OnInit {
-  games: any[] = [];
 
-  constructor(private gamingService: GamingService) { }
-
-  ngOnInit() {
-    this.gamingService.getGames().subscribe((data: any) => {
-      this.games = data.results; // Assuming 'results' contains the array of games
-    });
-  }
+export class HomePage {
+  constructor() {}
 }
