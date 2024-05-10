f68d66e7f613888545e1790840b6612405ca4c33
Fionn McGoldrick <G00422349@atu.com>
2024-05-10 03:03:51 +0000

    Game service html editted and ts updated

--- src/app/game-list/game-list.page.ts
+++ src/app/game-list/game-list.page.ts
@@ -1,20 +1,29 @@
+import { GamingService } from '../services/gaming.service';
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonLabel } from '@ionic/angular/standalone';
+import { IonItem } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-game-list',
   templateUrl: './game-list.page.html',
   styleUrls: ['./game-list.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel],
 })
 export class GameListPage implements OnInit {
+  games: any[] = [];
 
-  constructor() { }
+  constructor(private apiService: GamingService) {}
 
   ngOnInit() {
+    this.loadGames();
+  }
+
+  loadGames() {
+    this.apiService.getGames().toPromise().then((response: any) => {
+      this.games = response; // Assuming the response is already JSON
+    });
   }
 
 }
